3caa5ddcc2afc54cfca4c37ff281c8ad0f676c15
Benedikt Grande <benedikt.grande@mayflower.de>
2013-08-22 15:39:57 +0200

    use objects

--- app.js
+++ app.js
@@ -7,21 +7,176 @@
  * @author    Diana Hartmann <diana.hartmann@mayflower.de>
  */
 
-var port, app, server, io, storage;
+var port, app, server, io, storage, helpers;
 
 port = process.env.PORT || 3000;
 app = require('express')();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
+
+/* @todo move objects into own module */
+helpers = {
+    /**
+     * helper function to count object lists
+     *
+     * @param object object
+     *
+     * @returns int
+     */
+    'countObject': function(object) {
+        var count = 0;
+        for (x in object) {
+            count++;
+        }
+        return count;
+    },
+
+    /**
+     * @TODO move into helper used by server and frontend app parts
+     *
+     * @param variable
+     *
+     * @returns boolean
+     */
+    'isSet': function(variable) {
+        return !(null === variable || undefined === variable || "" === variable);
+    }
+};
+
 storage = {
-    'sessions': {},
     'admin': null,
     'isOpen': true,
     'users': {},
     'desks': [],
     'allowedCardValues': [
         0, '1/2', 1, 2, 3, 5, 8, 13, 20, 40, 100, '?'
-    ]
+    ],
+    'helper': null,
+    /**
+     * function to map list of userId identified uers to array
+     *
+     * @param userId string
+     *
+     * @returns array
+     */
+    'getUsers': function (userId) {
+        var userList = {
+                'users': []
+            },
+            i = 0;
+
+        for (x in this.users) {
+            if (!helpers.isSet(userList.users[i])) {
+                userList.users[i] = {};
+            }
+
+            userList.users[i].username = this.users[x].username;
+            userList.users[i].admin = this.users[x].admin;
+
+            if (helpers.isSet(this.users[x].cardValue)) {
+                userList.users[i].cardValue = '...';
+            }
+
+            if (userId === x || false === this.isOpen) {
+                userList.users[i].cardValue = this.users[x].cardValue;
+            }
+            i++;
+        }
+
+        return userList;
+    },
+    /**
+     * function to update the username
+     *
+     * @param data string
+     * @param id string
+     *
+     * @returns void
+     */
+    'updateUsername': function (data, id) {
+        if ('string' == typeof data) {
+            if (!this.users[id]) {
+                this.users[id] = {};
+            }
+            this.users[id].username = data;
+        }
+    },
+    /**
+     * function to map cardValues from userIds to usernames
+     *
+     * @param cards object
+     *
+     * @returns object
+     */
+    'getCardValuesByUsername': function (cards) {
+        var cardList = {};
+    
+        for (x in cards) {
+            if (this.users[x]) {
+                var name = this.users[x].username;
+                cardList[name] = cards[x];
+            }
+        }
+    
+        return cardList;
+    },
+    /**
+     * helper function to check if card value is allowed
+     *
+     * @param data string
+     *
+     * @returns bool
+     */
+    'checkCardValue': function (data) {
+        for (var i = 0; i < this.allowedCardValues.length; i++) {
+            if (data == this.allowedCardValues[i]) {
+                return true;
+            }
+        }
+        return false;
+    },
+    /**
+     * helper function to check if all cards are already set
+     *
+     * @param deskCount int
+     *
+     * @returns bool
+     */
+    'checkIfAllCardsSet': function (deskCount) {
+        var cardCount, userCount, users, cards;
+        
+        cardCount = userCount = 0;
+        users = this.users;
+        cards = this.desks[deskCount - 1].cards;
+        
+        userCount = helpers.countObject(users);
+        cardCount = helpers.countObject(cards);
+        
+        return userCount === cardCount;
+    },
+    /**
+     * helper function to check if username is already set
+     *
+     * @param data string
+     *
+     * @returns bool
+     */
+    'checkUsername': function (data) {
+        for (x in this.users) {
+            if (data == this.users[x].username) {
+                return false;
+            }
+        }
+        return true;
+    },
+    'setAdmin': function(userId) {
+        if (helpers.isSet(userId)) {
+            this.admin = userId;
+        }
+    },
+    'getAdmin': function() {
+        return this.admin;  
+    }    
 };
 
 // switch to xhr polling for heroku and disable debug output
@@ -83,7 +238,7 @@ io.sockets.on('connection', function(socket) {
         } 
         
         // if we got a new userId update the user and kill the old one!
-        if (oldUserId !== userId && isSet(storage.users[oldUserId])) {
+        if (oldUserId !== userId && helpers.isSet(storage.users[oldUserId])) {
             var desk = storage.desks.length - 1,
                 card = storage.desks[desk].cards[oldUserId];
             
@@ -101,26 +256,28 @@ io.sockets.on('connection', function(socket) {
             }
         }
     
-        if (!checkUsername(username)) {
+        if (!storage.checkUsername(username)) {
             //noinspection JSUnusedAssignment
             data += '2';
         }
-        updateUsername(username, userId);
+        storage.updateUsername(username, userId);
         
         if (null !== cardValue) {
             storage.users[userId].cardValue = cardValue;
         }
+
+        var userList = storage.getUsers(userId);
         
         socket.emit('users', {
             'userId': userId,
-            'users': getUsers(userId)
+            'users': userList
         });
-        socket.broadcast.emit('users', getUsers(userId))
+        socket.broadcast.emit('users', userList)
     });
 
     socket.on('isOpen', function(data) {
         var response = false;
-        if (isSet(data) && true === storage.isOpen) {
+        if (helpers.isSet(data) && true === storage.isOpen) {
             response = data;
         }
         socket.emit('isOpenSuccess', response);
@@ -129,7 +286,7 @@ io.sockets.on('connection', function(socket) {
     socket.on('setCard', function(data) {
         var deskCount = storage.desks.length;        
         
-        if (!checkCardValue(data)) {
+        if (!storage.checkCardValue(data)) {
             socket.emit('sendCard', false);
             return;
         }
@@ -149,8 +306,8 @@ io.sockets.on('connection', function(socket) {
         socket.emit('sendCard', data);
 
         var closedDesk;
-        if (checkIfAllCardsSet(deskCount)) {
-            var cardValues = getCardValuesByUsername(storage.desks[deskCount - 1].cards);
+        if (storage.checkIfAllCardsSet(deskCount)) {
+            var cardValues = storage.getCardValuesByUsername(storage.desks[deskCount - 1].cards);
             storage.isOpen = false;
             closedDesk = {
                 'desk': deskCount,
@@ -162,27 +319,28 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('changeUsername', function(data) {
-        if (!isSet(data)) {
+        if (!helpers.isSet(data)) {
             socket.emit('users', {
                 'error': 'username not allowed!'
             });
             return;
         }
         
-        if (!checkUsername(data)) {
+        if (!storage.checkUsername(data)) {
             socket.emit('users', {
                 'error': 'username already in use!'
             });            
             return;
         } 
         
-        updateUsername(data, userId);       
+        storage.updateUsername(data, userId);      
+        var userList = storage.getUsers(userId);
         
         socket.emit('users', {
             'userId': userId,
-            'users': getUsers(userId)
+            'users': userList
         });
-        socket.broadcast.emit('users', getUsers(userId));
+        socket.broadcast.emit('users', userList);
     });
 
     socket.on('disconnect', function (data) {
@@ -199,155 +357,3 @@ console.log('disconnect:', data);
     // edit/add title/name
 });
 
-
-// @todo these functions should be part of a (helper) module
-/**
- * function to map list of userId identified uers to array
- *
- * @param userId string
- *
- * @returns array
- */
-function getUsers(userId) {
-    var userList = {
-            'users': []
-        },
-        i = 0;
-    
-    for (x in storage.users) {
-        if (!userList.users[i]) {
-            userList.users[i] = {};
-        }
-        
-        userList.users[i].username = storage.users[x].username;
-        userList.users[i].admin = storage.users[x].admin;
-        
-        if (isSet(storage.users[x].cardValue)) {
-            userList.users[i].cardValue = '...';
-        }
-
-        if (userId === x || false === storage.isOpen) {       
-            userList.users[i].cardValue = storage.users[x].cardValue;
-        }
-        i++;
-    }
-
-    return userList;
-}
-
-
-/**
- * function to update the username
- *
- * @param data string
- * @param id string
- *
- * @returns void
- */
-function updateUsername(data, id) {
-    if ('string' == typeof data) {
-        if (!storage.users[id]) {
-            storage.users[id] = {};
-        }
-        storage.users[id].username = data;
-    }
-}
-
-
-/**
- * function to map cardValues from userIds to usernames
- *
- * @param cards object
- *
- * @returns object
- */
-function getCardValuesByUsername(cards) {
-    var cardList = {};
-    
-    for (x in cards) {
-        if (storage.users[x]) {
-            var name = storage.users[x].username;
-            cardList[name] = cards[x];
-        }
-    }
-    
-    return cardList;
-}
-
-/**
- * helper function to check if card value is allowed
- *
- * @param data string
- *
- * @returns bool
- */
-function checkCardValue(data) {
-    for (var i = 0; i < storage.allowedCardValues.length; i++) {
-        if (data == storage.allowedCardValues[i]) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
- * helper function to check if all cards are already set
- *
- * @param deskCount int
- *
- * @returns bool
- */
-function checkIfAllCardsSet(deskCount) {
-    var cardCount, userCount, users, cards;
-    
-    cardCount = userCount = 0;
-    users = storage.users;
-    cards = storage.desks[deskCount - 1].cards;
-    
-    userCount = countObject(users);
-    cardCount = countObject(cards);
-
-    return userCount === cardCount;   
-}
-
-/**
- * helper function to count object lists
- *
- * @param object object
- *
- * @returns int
- */
-function countObject(object) {
-    var count = 0;
-    for (x in object) {
-        count++;
-    }
-    return count;
-}
-
-/**
- * helper function to check if username is already set
- *
- * @param data string
- *
- * @returns bool
- */
-function checkUsername(data) {
-    for (x in storage.users) {
-        if (data == storage.users[x].username) {
-            return false;
-        }
-    }
-    return true;
-}
-
-/**
- * @TODO move into helper used by server and frontend app parts
- * 
- * @param variable
- * 
- * @returns boolean
- */
-function isSet(variable) {
-    return !(null === variable || undefined === variable || "" === variable);
-}
